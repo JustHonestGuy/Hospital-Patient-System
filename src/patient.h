5c4c60ff13579b3e3b90a697118db5471931187b
JustHonestGuy <setu89628@gmail.com>
2025-11-30 18:30:50 +0500

    constructor in patient.h

--- src/patient.h
+++ src/patient.h
@@ -1,1 +1,22 @@
+#ifndef PATIENT_H
+#define PATIENT_H
 
+#include <string>
+
+struct Patient {
+    int id;
+    std::string name;
+    int severity;
+    long long arrivalTime;
+
+    Patient(int id, std::string name, int severity, long long arrivalTime) {
+        this->id = id;
+        this->name = name;
+        this->severity = severity;
+        this->arrivalTime = arrivalTime;
+    }
+
+    Patient() {}
+};
+
+#endif
