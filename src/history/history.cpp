a5585043609eb8bc239c33098b12dd3d3ac49164
JustHonestGuy <setu89628@gmail.com>
2025-11-30 18:26:02 +0500

    history

--- src/history/history.cpp
+++ src/history/history.cpp
@@ -1,1 +1,26 @@
+#include "history.h"
+#include <iostream>
+
+HistoryList::HistoryList() {
+    head = tail = nullptr;
+}
+
+void HistoryList::add(Patient p) {
+    HistoryNode* node = new HistoryNode(p);
+    if (!head) {
+        head = tail = node;
+    } else {
+        tail->next = node;
+        tail = node;
+    }
+}
+
+void HistoryList::display() {
+    HistoryNode* cur = head;
+    while (cur) {
+        std::cout << cur->data.name 
+                  << " (sev=" << cur->data.severity << ")\n";
+        cur = cur->next;
+    }
+}
 
