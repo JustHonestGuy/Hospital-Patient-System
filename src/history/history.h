9cb9a838eca4ff177de528ff8f37287f5f563b4a
JustHonestGuy <setu89628@gmail.com>
2025-11-30 18:26:28 +0500

    headH

--- src/history/history.h
+++ src/history/history.h
@@ -1,1 +1,28 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include "../patient.h"
+
+struct HistoryNode {
+    Patient data;
+    HistoryNode* next;
+
+    HistoryNode(Patient p) {
+        data = p;
+        next = nullptr;
+    }
+};
+
+class HistoryList {
+private:
+    HistoryNode* head;
+    HistoryNode* tail;
+
+public:
+    HistoryList();
+    void add(Patient p);
+    void display();
+};
+
+#endif
 
